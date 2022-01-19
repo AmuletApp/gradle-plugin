2afbdba23bc2b8980e897612404258657d5a7886
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-01-19 07:16:34 -0800

    feat: finish reddit dependency

--- src/main/kotlin/com/github/redditvanced/gradle/RedditVancedPlugin.kt
+++ src/main/kotlin/com/github/redditvanced/gradle/RedditVancedPlugin.kt
@@ -2,6 +2,8 @@ package com.github.redditvanced.gradle
 
 import com.github.redditvanced.gradle.models.PluginManifest
 import com.github.redditvanced.gradle.models.RemoteData
+import com.googlecode.d2j.dex.Dex2jar
+import com.googlecode.d2j.reader.MultiDexFileReader
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionContainer
@@ -10,12 +12,15 @@ import org.gradle.api.provider.Property
 import org.gradle.internal.impldep.com.google.gson.Gson
 import java.io.*
 import java.net.URL
+import java.nio.file.Files
+import java.util.zip.ZipFile
 
 private const val DATA_URL = "https://raw.githubusercontent.com/RedditVanced/RedditVanced/builds/data.json"
+private const val APK_URL = "https://redditvanced.app/reddit/%s"
 
 abstract class RedditVancedPlugin : Plugin<Project> {
 	override fun apply(project: Project) {
-		project.extensions.create("redditvanced", RedditVancedExtension::class.java, project)
+		project.extensions.create("redditVanced", RedditVancedExtension::class.java, project)
 
 		val cacheDir = File(project.gradle.gradleUserHomeDir, "caches/redditvanced")
 		project.configurations.register("redditApk") {
@@ -23,14 +28,14 @@ abstract class RedditVancedPlugin : Plugin<Project> {
 		}
 		project.afterEvaluate {
 			val dependencies = project.configurations.getByName("redditApk").dependencies
-			val extension = project.extensions.getRedditVanced()
+			val gson = Gson()
 
 			dependencies.forEach {
 				val version = when (it.version) {
 					"SNAPSHOT" -> {
 						project.logger.lifecycle("Fetching core's reddit version")
 						val reader = BufferedReader(InputStreamReader(URL(DATA_URL).openStream()))
-						val data = Gson().fromJson(reader, RemoteData::class.java)
+						val data = gson.fromJson(reader, RemoteData::class.java)
 
 						project.logger.lifecycle("Fetched Reddit version: ${data.latestRedditVersionName} (${data.latestRedditVersionCode})")
 						data.latestRedditVersionCode
@@ -38,16 +43,54 @@ abstract class RedditVancedPlugin : Plugin<Project> {
 					else -> it.version
 				}
 
+				val tmpApkFile = File(System.getProperty("java.io.tmpdir"), "reddit-${version}-tmp.jar")
 				val apkFile = File(cacheDir, "reddit-${version}.apk")
 				val jarFile = File(cacheDir, "reddit-${version}.jar")
 				cacheDir.mkdirs()
 
 				if (!apkFile.exists()) {
-					val url =
+					project.logger.lifecycle("Downloading Reddit APK")
+					val url = URL(APK_URL.format(version))
+					val reader = BufferedReader(InputStreamReader(url.openStream()))
+					val data = gson.fromJson(reader, RedditAPK::class.java)
+
+					val apkUrl = URL(data.downloadUrl)
+					apkUrl.download(tmpApkFile, createProgressLogger(project, "Download Reddit APK"))
+
+					project.logger.lifecycle("Extracting main apk from xapk")
+					val zip = ZipFile(tmpApkFile)
+					val stream = zip.getInputStream(zip.getEntry("com.reddit.frontpage.apk"))
+
+					apkFile.outputStream().use { outStream ->
+						stream.copyTo(outStream)
+						outStream.close()
+					}
+					stream.close()
+					tmpApkFile.delete()
 				}
+
+				if (!jarFile.exists()) {
+					project.logger.lifecycle("Converting APK to jar")
+
+					val reader = MultiDexFileReader.open(Files.readAllBytes(jarFile.toPath()))
+					Dex2jar.from(reader)
+						.skipDebug(false)
+						.topoLogicalSort()
+						.noCode(true)
+						.to(jarFile) // TODO: kotlin pls stop
+				}
+
+				project.dependencies.add("compileOnly", project.files(jarFile))
 			}
 		}
 	}
+
+	private data class RedditAPK(
+		val versionCode: String,
+		val versionName: String,
+		val size: Int,
+		val downloadUrl: String,
+	)
 
 	fun ExtensionContainer.getRedditVanced() =
 		getByType(RedditVancedExtension::class.java)
