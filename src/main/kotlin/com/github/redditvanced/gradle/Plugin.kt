65bd5606e85f11f65eeca9c334ee17179c710414
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-02-27 13:48:34 -0800

    feat: new injector build method

--- src/main/kotlin/com/github/redditvanced/gradle/Plugin.kt
+++ src/main/kotlin/com/github/redditvanced/gradle/Plugin.kt
@@ -8,9 +8,10 @@ import groovy.json.JsonBuilder
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.AbstractCopyTask
-import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.bundling.Zip
 import org.gradle.api.tasks.compile.AbstractCompile
+import java.io.File
+import java.util.zip.ZipFile
 
 private const val TASK_GROUP = "reddit vanced"
 
@@ -34,24 +35,25 @@ abstract class Plugin : Plugin<Project> {
 		val extension = project.extensions.getRedditVanced()
 		val projectType = extension.projectType.get()
 
-		project.tasks.register("compileResources", CompileResourcesTask::class.java) { task ->
-			task.group = TASK_GROUP
+		if (projectType != ProjectType.INJECTOR) {
+			project.tasks.register("compileResources", CompileResourcesTask::class.java) { task ->
+				task.group = TASK_GROUP
 
-			val processManifestTask = project.tasks.getByName("processDebugManifest") as ProcessLibraryManifest
-			task.dependsOn(processManifestTask)
+				val processManifestTask = project.tasks.getByName("processDebugManifest") as ProcessLibraryManifest
+				task.dependsOn(processManifestTask)
 
-			task.input.set(android.sourceSets.getByName("main").res.srcDirs.single())
-			task.manifestFile.set(processManifestTask.manifestOutputFile)
+				task.input.set(android.sourceSets.getByName("main").res.srcDirs.single())
+				task.manifestFile.set(processManifestTask.manifestOutputFile)
+				task.outputFile.set(intermediates.resolve("res.apk"))
 
-			task.outputFile.set(intermediates.resolve("res.apk"))
+				task.doLast { _ ->
+					val resApkFile = task.outputFile.asFile.get()
 
-			task.doLast { _ ->
-				val resApkFile = task.outputFile.asFile.get()
-
-				if (resApkFile.exists()) {
-					project.tasks.named("make", AbstractCopyTask::class.java) {
-						it.from(project.zipTree(resApkFile)) { copySpec ->
-							copySpec.exclude("AndroidManifest.xml")
+					if (resApkFile.exists()) {
+						project.tasks.named("make", AbstractCopyTask::class.java) {
+							it.from(project.zipTree(resApkFile)) { copySpec ->
+								copySpec.exclude("AndroidManifest.xml")
+							}
 						}
 					}
 				}
@@ -74,67 +76,90 @@ abstract class Plugin : Plugin<Project> {
 			task.group = TASK_GROUP
 		}
 
-		val makeTaskType = when (projectType) {
-			ProjectType.INJECTOR -> Copy::class.java
-			else -> Zip::class.java
-		}
+		if (projectType == ProjectType.INJECTOR) {
+			project.tasks.register("make", Zip::class.java) { task ->
+				task.group = TASK_GROUP
+				task.dependsOn(project.tasks.getByName("build"))
 
-		project.tasks.register("make", makeTaskType) { task ->
-			task.group = TASK_GROUP
+				val apkFile = File(project.buildDir, "outputs/apk/release/Injector-release-unsigned.apk")
+				val tempDir = File(project.buildDir, "intermediates/make")
+					.apply { mkdirs() && File(this, "tmp").createNewFile() }
 
-			val compileDexTask = project.tasks.getByName("compileDex") as CompileDexTask
-			task.dependsOn(compileDexTask)
+				task.from(tempDir)
+				task.destinationDirectory.set(project.buildDir)
+				task.isPreserveFileTimestamps = false
+				task.archiveBaseName.set(project.name)
+				task.archiveVersion.set("")
 
-			if (projectType == ProjectType.PLUGIN || projectType == ProjectType.CORE) {
-				val manifestFile = intermediates.resolve("manifest.json")
-
-				task.from(manifestFile)
 				task.doFirst {
-					require(project.version != "unspecified") { "No project version is set!" }
+					// Create file to force task to run, and now exclude it
+					task.exclude("tmp")
 
-					val manifest: Any = if (projectType == ProjectType.PLUGIN) {
-						PluginManifest(
-							name = project.name,
-							version = project.version.toString(),
-							pluginClass = extension.pluginClass.get(),
-							changelog = extension.changelog.getOrElse(""),
-							description = project.description ?: "",
-							authors = extension.authors.get(),
-							customUpdaterUrl = extension.customUpdaterUrl.orNull,
-							loadResources = extension.loadResources.getOrElse(false),
-							requiresRestart = extension.requiresRestart.getOrElse(false),
-						)
-					} else {
-						CoreManifest(
-							version = project.version.toString(),
-							redditVersionCode = extension.projectRedditVersion.get()
-						)
+					val apk = ZipFile(apkFile)
+					for (entry in apk.entries().asIterator()) {
+						if (!entry.name.startsWith("lib/") && !entry.name.startsWith("classes"))
+							continue
+
+						apk.getInputStream(entry).use {
+							File(tempDir, entry.name).apply {
+								if (!exists()) {
+									parentFile.mkdirs()
+									createNewFile()
+								}
+								writeBytes(it.readAllBytes())
+							}
+						}
 					}
-					manifestFile.writeText(JsonBuilder(manifest).toString())
+
+					apk.close()
 				}
 			}
+		} else {
+			project.tasks.register("make", Zip::class.java) { task ->
+				task.group = TASK_GROUP
 
-			task.from(compileDexTask.outputFile)
+				val compileDexTask = project.tasks.getByName("compileDex") as CompileDexTask
+				task.dependsOn(compileDexTask)
 
-			if (projectType == ProjectType.INJECTOR) {
-				task.into(project.buildDir)
-				task.rename { "Injector.dex" }
+				if (projectType == ProjectType.PLUGIN || projectType == ProjectType.CORE) {
+					val manifestFile = intermediates.resolve("manifest.json")
 
-				// Retarded gradle won't remove the target dir as an output
-				// Absolutely fuming
-				task.outputs.file("${project.buildDir}/Injector.dex")
-			} else {
-				task as Zip
+					task.from(manifestFile)
+					task.doFirst {
+						require(project.version != "unspecified") { "No project version is set!" }
 
+						val manifest: Any = if (projectType == ProjectType.PLUGIN) {
+							PluginManifest(
+								name = project.name,
+								version = project.version.toString(),
+								pluginClass = extension.pluginClass.get(),
+								changelog = extension.changelog.getOrElse(""),
+								description = project.description ?: "",
+								authors = extension.authors.get(),
+								customUpdaterUrl = extension.customUpdaterUrl.orNull,
+								loadResources = extension.loadResources.getOrElse(false),
+								requiresRestart = extension.requiresRestart.getOrElse(false),
+							)
+						} else {
+							CoreManifest(
+								version = project.version.toString(),
+								redditVersionCode = extension.projectRedditVersion.get()
+							)
+						}
+						manifestFile.writeText(JsonBuilder(manifest).toString())
+					}
+				}
+
+				task.from(compileDexTask.outputFile)
 				task.dependsOn(project.tasks.getByName("compileResources"))
 				task.isPreserveFileTimestamps = false
 				task.archiveBaseName.set(project.name)
 				task.archiveVersion.set("")
 				task.destinationDirectory.set(project.buildDir)
-			}
 
-			task.doLast {
-				task.logger.lifecycle("Made package at ${task.outputs.files.first().absolutePath}")
+				task.doLast {
+					task.logger.lifecycle("Made package at ${task.outputs.files.first().absolutePath}")
+				}
 			}
 		}
 
